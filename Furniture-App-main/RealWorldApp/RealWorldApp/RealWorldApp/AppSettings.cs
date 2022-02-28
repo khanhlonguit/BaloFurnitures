a411decb2690d22ccf27b82d723075c083eaefa8
khanhlonguit <longvuongbrvt@gmail.com>
2022-02-28 13:12:43 +0700

    first commit

---
+++ Furniture-App-main/RealWorldApp/RealWorldApp/RealWorldApp/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealWorldApp
+{
+    public static class AppSettings
+    {
+        public static string ApiUrl = "https://furnitureapp.azurewebsites.net/";
+    }
+}
+  
